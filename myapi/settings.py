e5ddb9191a30dfd71aad0ac9b5745c9587430c9b
Mudasir Hanif <ms03831@st.habib.edu.pk>
2021-03-18 22:08:55 +0500

    django heroku added

--- myapi/settings.py
+++ myapi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -133,6 +133,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 #
@@ -141,3 +142,4 @@ STATIC_URL = '/static/'
 
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
